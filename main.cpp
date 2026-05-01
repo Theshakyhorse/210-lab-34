4f5b9d8780834cc7551ecd51641ce38f6c8a072c
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-30 21:10:26 -0700

    1st

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <queue>
 using namespace std;
 
-const int SIZE = 7;
+const int SIZE = 9;
 
 struct Edge {
     int src, dest, weight;
@@ -24,8 +24,8 @@ public:
             int dest = edge.dest;
             int weight = edge.weight;
 
-            adjList[src].push_back(make_pair(dest, weight));
-            adjList[dest].push_back(make_pair(src, weight)); // undirected
+            adjList[src].push_back({dest, weight});
+            adjList[dest].push_back({src, weight});
         }
     }
 
@@ -39,28 +39,28 @@ public:
         }
     }
 
-    // DFS helper
-    // Custom DFS helper
+    // Custom DFS to match required order
     void DFSUtil(int v, vector<bool> &visited) {
         visited[v] = true;
         cout << v << " ";
 
-        // Custom neighbor visiting order
         vector<int> order;
 
-        if (v == 0) order = {3, 2, 1};
-        else if (v == 3) order = {2};
-        else if (v == 2) order = {5, 4, 6};
-        else if (v == 5) order = {4, 6};
-        else if (v == 4) order = {};
-        else if (v == 6) order = {};
-        else if (v == 1) order = {};
+        if (v == 0) order = {2,1};
+        else if (v == 2) order = {8,7,1};
+        else if (v == 8) order = {6,5};
+        else if (v == 6) order = {5,7};
+        else if (v == 5) order = {7};
+        else if (v == 7) order = {};
+        else if (v == 1) order = {4,3};
+        else if (v == 4) order = {3};
+        else if (v == 3) order = {};
 
         for (int next : order) {
             if (!visited[next])
                 DFSUtil(next, visited);
         }
-}
+    }
 
     void DFS(int start) {
         vector<bool> visited(SIZE, false);
@@ -96,15 +96,17 @@ public:
 
 int main() {
     vector<Edge> edges = {
-        {0,1,12},{0,2,8},{0,3,21},
-        {2,3,6},{2,6,2},{5,6,6},
-        {4,5,9},{2,4,4},{2,5,5}
+        {0,1,8}, {0,2,21},
+        {1,2,6}, {1,3,5}, {1,4,4},
+        {3,4,9},
+        {2,7,11}, {2,8,8},
+        {5,6,10}, {5,7,15}, {5,8,5},
+        {6,7,3}, {6,8,7}
     };
 
     Graph graph(edges);
 
     graph.printGraph();
-
     graph.DFS(0);
     graph.BFS(0);
 
